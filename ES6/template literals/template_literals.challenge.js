c5e80c1799433b94e3b820e8fa13eb7146bfd170
Dhruv Bhatnagar <Dhruv_Bhatnagar@epam.com>
2019-07-24 14:43:45 +0530

    3,4 Challenges done

--- ES6/template literals/template_literals.challenge.js
+++ ES6/template literals/template_literals.challenge.js
@@ -2,4 +2,36 @@
 // check if 
 // if a registered member - discount is 5% 
 // if not registered member - discount is 4%
+let personData = [
+    {
+        registered: true,
+        name: "Dhruv",
+        age: 21
+    },
+    {
+        registered: false,
+        name: "Rajdeep",
+        age: 21
+    },
+    {
+        registered: true,
+        name: "Nikhil",
+        age: 21
+    }
+]
+function calculateDiscount(person) {
+    let discounts;
+    discounts = person.map(function (currentValue) {
+        if (currentValue.registered == true)
+            return `Discount for ${currentValue.name} is 5%`;
+        else
+            return `Discount for ${currentValue.name} is 4%`;
+    });
+    return discounts;
+}
+function returnDiscount(strings, func) {
+    return func(personData);
+}
 
+const discount = returnDiscount`${() => calculateDiscount(personData)}`;
+console.log(discount.join("\n"));
