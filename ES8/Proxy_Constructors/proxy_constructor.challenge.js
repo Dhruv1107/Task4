dd37667ee16470828957ae7e0e6d84d00c396dc9
Dhruv Bhatnagar <Dhruv_Bhatnagar@epam.com>
2019-07-24 17:26:50 +0530

    5th Challenge done

--- ES8/Proxy_Constructors/proxy_constructor.challenge.js
+++ ES8/Proxy_Constructors/proxy_constructor.challenge.js
@@ -12,11 +12,28 @@ let manager = {
     employees: 0
 }
 
-manager.office = `London` //updates
-manager.employees = ['Jim', 'Patrick', 'Mary']; //updates
-manager.employees = 3; // doesn't update
-manager.employees = null; // updates
-manager.employees = {name:'Jim'} // doesn't update
+let handler = {
+    set: (manager, propName, newValue) => {
+        if (propName === "employees") {
+            if (Array.isArray(newValue) || typeof newValue === 'string' || newValue == null)
+                manager[propName] = newValue;
+            else
+                console.log("Error cannot change");
+        }
+        else
+            manager[propName] = newValue;
+    }
+}
+
+let empObject = new Proxy(manager, handler);
+// empObject.employees = ['Ram'];
+empObject.office = `London` //updates
+empObject.employees = ['Jim', 'Patrick', 'Mary']; //updates
+console.log(empObject);
+empObject.employees = 3; // doesn't update
+empObject.employees = null; // updates
+empObject.employees = { name: 'Jim' } // doesn't update
+console.log(empObject);
 
 // 2. GET
 // adjust the following code so that anytime an internal object with accessLevel of 1 is accessed,
@@ -38,9 +55,27 @@ const users = [
         accessLevel: 2,
         accessCode: 9999
     }
-]
+];
 
-console.log(users[0].username)  // Access Denied
-console.log(users[0].accessCode) // Access Denied
-console.log(users[1].accessCode) // 2345
-console.log(users[2].username) // Harold
+let handler1 = {
+    get: (target, propName) => {
+        if (target[propName].accessLevel === 1)
+            return `Access Denied`;
+        else
+            return target[propName];
+    }
+}
+
+getMessage = (msg) => {
+    if (msg == undefined)
+        console.log("Access Denied");
+    else
+        console.log(msg);
+}
+
+let usersObj = new Proxy(users, handler1);
+
+getMessage(usersObj[0].username)  // Access Denied
+getMessage(usersObj[0].accessCode) // Access Denied
+getMessage(usersObj[1].accessCode) // 2345
+getMessage(usersObj[2].username) // Harold
